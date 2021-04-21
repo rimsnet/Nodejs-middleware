e6190ba9834226f5c49bd7f230bdb92931cac620
Rimsan <rimsnet@gmail.com>
2021-04-21 15:39:48 +0800

    Add custom middleware

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const PORT    = process.env.PORT || 3000;
 
 app.use(express.json());
 
+//custom middleware
+const checkUserMiddleware = (req, res, next) => {
+   const username = req.body.username;
+   if(username == 'usersa'){
+      res.json({err:'Yo, we dont allow usersa'})
+   }else{
+      next();
+   }
+};
+
+//middleware apply for every single requrests
+/* app.use((req,res,next)=>{
+   console.log("Middleware called");
+   console.log(req.body);
+   next();
+}); */
 
 app.get('/',(req,res)=>{
    res.json({message:"hello World"});
+});
+
+//use custom middleware
+app.post('/', checkUserMiddleware, (req, res)=>{
+   res.send('You logged In!')
 });
 
 app.listen(PORT,()=>{console.log(`App is running on ${PORT}`)});
